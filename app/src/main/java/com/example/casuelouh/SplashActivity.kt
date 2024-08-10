de54ad7a99bb57dbbb1e3d1b6225e7417cf8c764
Ahmed Hussein <ahkcsit@gmail.com>
2024-08-10 12:28:43 +0300

    Add app identity

---
+++ app/src/main/java/com/example/casuelouh/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.casuelouh
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay for 2 seconds and then start the MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close SplashActivity so user can't return to it
+        }, 2000) // Adjust delay as needed (2000 ms = 2 seconds)
+    }
+}
